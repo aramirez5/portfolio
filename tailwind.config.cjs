419462fd7ae00359e1404202313500ba6b090480
Álvaro Ramírez Lorente <e_arimir@mercadona.com>
2024-04-02 18:20:10 +0200

    Add light/dark theme button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
       fontFamily: {
@@ -12,8 +12,7 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+		themes: ["light", "dark"],
+		logs: false
 	}
 }
